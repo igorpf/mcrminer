70053bd17ec42f48094938b3a2a95678a44bb4a9
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2018-07-11 20:27:17 -0300

    First commit

---
+++ src/main/java/com/mcrminer/McrminerApplication.java
@@ -0,0 +1,12 @@
+package com.mcrminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McrminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McrminerApplication.class, args);
+	}
+}
