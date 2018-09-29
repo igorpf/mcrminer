57266dae1071e2fc02adfd66720e73444c0b13c9
Igor Ferreira <ipferreira@inf.ufrgs.br>
2018-09-29 14:15:56 -0300

    Removed useless getters/setters

--- src/main/java/com/mcrminer/model/projections/ReviewRequestProjection.java
+++ src/main/java/com/mcrminer/model/projections/ReviewRequestProjection.java
@@ -1,70 +1,19 @@
 package com.mcrminer.model.projections;
 
 import com.mcrminer.model.enums.ReviewRequestStatus;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.*;
 
 @Builder
-@AllArgsConstructor
+@Value
+@RequiredArgsConstructor
 public class ReviewRequestProjection {
-    private Integer projectId;
-    private String branch, commitId, description, submitterEmail;
-    private boolean isPublic;
-    private ReviewRequestStatus status;
-
-    public Integer getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Integer projectId) {
-        this.projectId = projectId;
-    }
-
-    public String getBranch() {
-        return branch;
-    }
-
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
-
-    public String getCommitId() {
-        return commitId;
-    }
-
-    public void setCommitId(String commitId) {
-        this.commitId = commitId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getSubmitterEmail() {
-        return submitterEmail;
-    }
-
-    public void setSubmitterEmail(String submitterEmail) {
-        this.submitterEmail = submitterEmail;
-    }
+    Integer projectId;
+    String branch, commitId, description, submitterEmail;
+    @Getter(AccessLevel.NONE)
+    boolean isPublic;
+    ReviewRequestStatus status;
 
     public boolean getIsPublic() {
         return isPublic;
-    }
-
-    public void setPublic(boolean aPublic) {
-        isPublic = aPublic;
-    }
-
-    public ReviewRequestStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ReviewRequestStatus status) {
-        this.status = status;
     }
 }
